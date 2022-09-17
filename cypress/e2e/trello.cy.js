a78eef6ac8522e78b57444c49974d73989f7f88e
Nijat Muhtar <nijatmn@gmail.com>
2022-09-17 10:39:51 -0400

    fixed the issue

--- cypress/e2e/trello.cy.js
+++ cypress/e2e/trello.cy.js
@@ -10,14 +10,26 @@ describe('smoke test class', () => {
       let fakePass = faker.internet.password();
 
       // test scipt
+      cy.log("User is visiting Trello page");
       cy.visit('https://trello.com');
       cy.xpath("(//a[text()='Log in'])[1]").click();
+
+      cy.log("User is entering username");
       cy.get('input#user').clear();
       cy.get('input#user').type(fakeUser);
       cy.wait(500);
+      cy.get('input#login').click();
+
+      cy.log("User is entering password");
       cy.get('input#password').clear();
       cy.get('input#password').type(fakePass);
-      cy.get('div#password-error > p').should('be.visible');
+      cy.wait(500);
+
+      cy.log("User is clicking login button");
+      cy.get('input#login').click()
+      cy.wait(500);
+
+      cy.log("Verifying that error message is displayed");
+      cy.get('div#error > p').should('be.visible');
    });
-   
 });
