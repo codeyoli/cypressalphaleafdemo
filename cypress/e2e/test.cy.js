4bc43cdcb657ca57376b24be7b193e3c4387604b
Nijat Muhtar <nijatmn@gmail.com>
2022-09-17 10:23:47 -0400

    second version

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,16 +3,8 @@
 const { faker } = require('@faker-js/faker');
 
 describe('smoke test class', () => {
-   let testdata;
 
-   beforeEach('setup', () => {
-      let fixture = cy.fixture('example');
-      fixture.then(function (data) {
-         testdata = data;
-      });
-   });
-
-   it('test case 1', () => {
+   it('Invalid login results in error message',  () => {
       // test data
       let fakeUser = faker.internet.email();
       let fakePass = faker.internet.password();
@@ -20,27 +12,12 @@ describe('smoke test class', () => {
       // test scipt
       cy.visit('https://trello.com');
       cy.xpath("(//a[text()='Log in'])[1]").click();
-      cy.get('#user').type(fakeUser);
-
-      testdata.email = 'nijat@alphaleafe.tech';
-
-      cy.get('#user').clear();
-      cy.get('#user').type(testdata.email);
+      cy.get('input#user').clear();
+      cy.get('input#user').type(fakeUser);
+      cy.wait(500);
+      cy.get('input#password').clear();
+      cy.get('input#password').type(fakePass);
+      cy.get('div#password-error > p').should('be.visible');
    });
-
-   it('test case 2',  () => {
-      // test data
-      let fakeUser = faker.internet.email();
-      let fakePass = faker.internet.password();
-
-      // test scipt
-      cy.visit('https://trello.com');
-      cy.xpath("(//a[text()='Log in'])[1]").click();
-      cy.get('#user').type(testdata.email);
-
-      testdata.email = 'nijat@alphaleafe.tech';
-
-      cy.get('#user').clear();
-      cy.get('#user').type(testdata.email);
-   });
+   
 });
